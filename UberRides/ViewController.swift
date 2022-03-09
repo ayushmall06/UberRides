352ec4da5f0721cca1d4cb75028949539f2fcfd0
Ayush Kumar Mall <65710495+ayushmall06@users.noreply.github.com>
2022-03-09 10:44:05 +0530

    Initial Commit

---
+++ UberRides/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UberRides
+//
+//  Created by Ayush Mall on 09/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
